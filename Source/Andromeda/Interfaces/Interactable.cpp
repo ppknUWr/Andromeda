bded74b94b30975078aa0cfd7221d6d4315b5d73
Kameleon <325135@uwr.edu.pl>
2022-10-18 18:37:42 +0200

    UI Character Statistic 2

---
+++ Source/Andromeda/Interfaces/Interactable.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactable.h"
+
+/*bool IInteractable::CanBeInteractedWith_Implementation(AModularCharacter* Character)
+{
+	return true;
+}*/
+
+// Add default functionality here for any IInteractable functions that are not pure virtual.
---
+++ Source/Andromeda/Interfaces/Interactable.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+//#include "../Character/ModularCharacter.h"
+#include "Interactable.generated.h"
+
+//forward declaration
+class AModularCharacter;
+
+// This class does not need to be modified.
+UINTERFACE()
+class UInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ANDROMEDA_API IInteractable
+{
+	GENERATED_BODY()
+	
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	bool CanBeInteractedWith(AModularCharacter* Character);
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void Interact(AModularCharacter* Character);
+};
