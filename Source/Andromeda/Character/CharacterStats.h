2693c9ed88c1308d0cbe74c567da0e5ef10861b8
technopop1 <igor01231@vp.pl>
2022-10-24 20:39:32 +0200

    Structures refactor

--- Source/Andromeda/Character/CharacterStats.h
+++ Source/Andromeda/Character/CharacterStats.h
@@ -28,3 +28,19 @@ struct FCharacterStats
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Basic Stats")
 	float Dexterity;
 };
+
+USTRUCT(BlueprintType)
+struct FWeaponExpGain
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Basic Stats")
+	float Sword = 10;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Basic Stats")
+	float Shield = 3;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Basic Stats")
+	float Bow = 5;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Basic Stats")
+	float Spear = 7;
+
+};
