7eabb404f9fd4890f473108a58979b3965230ecd
igor <323723@uwr.edu.pl>
2022-12-31 16:28:27 +0100

    Task#49 3d models and Task#50 magic partcile system added

---
+++ Source/Andromeda/Items/ThrowableWeaponItem.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThrowableWeaponItem.h"
+#include "Andromeda/Character/ModularCharacter.h"
+#include "Andromeda/Components/WeaponComponent.h"
+
+#define PrintInfo(String) GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Blue, String)
+
